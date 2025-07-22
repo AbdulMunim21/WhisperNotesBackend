01745510ae5a2243aefc7471f8fa98ecdf25fe47
AbdulMunim <amunim1999@gmail.com>
2025-07-22 11:53:26 +0500

    port changed

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ print("Model and tokenizer loaded successfully.")
 # Start server (use gunicorn in production)
 if __name__ == "__main__":
     print("Starting Flask server...")
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5050)
 
